ea95e51f080dd0c0d0aa5e86008a09785b1b7bed
jeffssant <santana.jeff@gmail.com>
2020-06-26 20:47:01 -0300

    Classe PageAdmin e templara para o admin

--- vendor/ecommerce/php-classes/src/Page.php
+++ vendor/ecommerce/php-classes/src/Page.php
@@ -12,16 +12,17 @@ class Page{
         'data' => []
     ];
 
-    public function __construct($opts = array()) // get header data
+    public function __construct($opts = array() , $tpl_dir = "/views/") // get header data
     {
         // Configurações padrão
-        // Config RainTPL
+        // Config RainTPL      
         $config = array(
-            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/ecommerce/views/",
-            "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/ecommerce/views-cache/",
+            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
+            "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
             "debug"         => false // set to false to improve the speed
            );
 
+           var_dump( $_SERVER['DOCUMENT_ROOT'].$tpl_dir);
         Tpl::configure( $config );
 
         // create the Tpl object
